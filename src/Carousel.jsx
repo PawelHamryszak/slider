c487bac07ab63486aef3755e9e93f994b22c53a3
PawelHamryszak <105717616+PawelHamryszak@users.noreply.github.com>
2023-05-10 15:40:32 +0200

    Update Carousel.jsx

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
-import { shortList, list, longList } from "./data";
-import { FaQuoteRight } from "react-icons/fa";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { useEffect, useState } from 'react';
+import { shortList, list, longList } from './data';
+import { FaQuoteRight } from 'react-icons/fa';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 const Carousel = () => {
   const [people, setPeople] = useState(longList);
-
   const [currentPerson, setCurrentPerson] = useState(0);
 
   const prevSlide = () => {
@@ -24,42 +23,41 @@ const Carousel = () => {
   useEffect(() => {
     let sliderId = setInterval(() => {
       nextSlide();
-    }, 2000);
+    }, 5000);
     return () => {
       clearInterval(sliderId);
     };
   }, [currentPerson]);
 
   return (
-    <section className="slider-container">
+    <section className='slider-container'>
       {people.map((person, personIndex) => {
         const { id, image, name, title, quote } = person;
         return (
           <article
-            className="slide"
+            className='slide'
             style={{
               transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
               opacity: personIndex === currentPerson ? 1 : 0,
-              visibility: personIndex === currentPerson ? "visible" : "hidden",
+              visibility: personIndex === currentPerson ? 'visible' : 'hidden',
             }}
             key={id}
           >
-            <img src={image} alt={name} className="person-img" />
-            <h5 className="name">{name}</h5>
-            <p className="title">{title}</p>
-            <p className="text">{quote}</p>
-            <FaQuoteRight className="icon" />
+            <img src={image} alt={name} className='person-img' />
+            <h5 className='name'>{name}</h5>
+            <p className='title'>{title}</p>
+            <p className='text'>{quote}</p>
+            <FaQuoteRight className='icon' />
           </article>
         );
       })}
-      <button type="button" className="prev" onClick={prevSlide}>
+      <button type='button' className='prev' onClick={prevSlide}>
         <FiChevronLeft />
       </button>
-      <button type="button" className="next" onClick={nextSlide}>
+      <button type='button' className='next' onClick={nextSlide}>
         <FiChevronRight />
       </button>
     </section>
   );
 };
-
 export default Carousel;
